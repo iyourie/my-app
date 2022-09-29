2b8b5b130516264109fe1754265d3643ba437cd7
iyourie <azhiedif@gmail.com>
2022-09-29 13:14:23 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import './App.css';
 let api = `http://www.omdbapi.com?apikey=41d901b9`
 
 function App() {
+
   async function search(title) {
     const res = await fetch(`${api}&s=${title}`)
     const data = await res.json()
     console.log(data)
   }
+
   useEffect(() => {
     search('batman')
   }, [])
+
   return (
-    
+    <h1>hello world</h1>
   )
 }
 
