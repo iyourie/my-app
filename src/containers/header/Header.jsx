c10e5b3679c2a831a31378264fecdd6f4d3367a0
iyourie <azhiedif@gmail.com>
2022-10-02 17:22:00 +0100

    .

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,16 +11,16 @@ let Header = () => {
                 <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
                 <div className='gpt3_header-content__input'>
-                    <input type="email" />
+                    <input type="email" placeholder='Your Email Adresse' />
                     <button>get started</button>
                 </div>
                 <div className="gpt3_header-content_people">
                     <img src={group81} alt="group81" />
                     <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
-                <div className="gpt3_header-image">
-                    <img src={AI} alt="" />
-                </div>
+            </div>
+            <div className="gpt3_header-image">
+                <img src={AI} alt="" />
             </div>
         </div>
     )
