6e0a26d0cf40821f31e89a9c47c45b49ee379a17
iyourie <azhiedif@gmail.com>
2022-10-02 07:29:58 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Blog, Features, Footer, Header, Possibility, WhatGP3 } from './containers'
 import { Article, Brand, Cta, Feature, Navbar } from './components'
+import './App.css'
 
 let App = () => {
   return (
-    <>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGP3 />
+      <Features />
+      <Possibility />
+      <Cta />
       <Blog />
-      <Article />
-    </>
+      <Footer />
+    </div>
   )
 }
 export default App;
