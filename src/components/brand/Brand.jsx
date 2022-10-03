9a6068988eca936fd2ac88af0ade43ea9d94ea60
iyourie <azhiedif@gmail.com>
2022-10-03 11:12:18 +0100

    .

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,28 @@
 import './brand.css';
+import { dropbox, shopify, google, slack, atlassian } from './import'
 
 
 
-function Brand() {
-
-    return <h1>brand</h1>
+const Brand = () => {
+    return (
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+            <div>
+                <img src={atlassian} alt="atlassian" />
+            </div>
+        </div>
+    )
 }
 
 export default Brand;
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,9 +2,19 @@ import './feature.css';
 
 
 
-function Feature() {
+function Feature({ title, text }) {
 
-    return <h1>fea</h1>
+    return (
+        <div className="gpt3__features-container_feature">
+            <div className="gpt3__features-container_feature-title">
+                <div />
+                <h1>{title}</h1>
+            </div>
+            <div className="gpt3__features-container_feature-text">
+                <p>{text}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Feature;
